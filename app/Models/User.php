338ee74364431bb1d10709376756db35203d5e9b
Yaroslaw Bagriy <yaroslaw.bagriy@evercode.io>
2021-11-15 07:21:46 -0600

    Episode 46 complete

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $guarded = []; 
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -36,6 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password) {
+        $this->attribute['password'] = bcrypt($password);
+    }
 
     public function posts() {
         return $this->hasMany(Post::class);
