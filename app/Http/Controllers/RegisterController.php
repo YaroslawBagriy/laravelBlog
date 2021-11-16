bd18b96b1fa72208c83fe5fdb582661b3b2b500f
Yaroslaw Bagriy <yaroslaw.bagriy@evercode.io>
2021-11-16 06:58:49 -0600

    Episode 47 complete

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,8 +15,8 @@ class RegisterController extends Controller
         // Create the user + form validation
         $attributes = request()->validate([
             'name' => ['required' , 'max:255'],
-            'username' => ['required' , 'max:255', 'min:3'],
-            'email' => ['required' , 'email', 'max:255'],
+            'username' => ['required' , 'max:255', 'min:3', Rule::unique('users', 'username')],
+            'email' => ['required' , 'email', 'max:255', Rule::unique('users', 'email')],
             'password' => ['required' , 'max:255', 'min:7']
         ]);
 
